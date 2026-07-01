6cd2407ef4b892fd5c0df09450e6f1d19d78205f
Yue Huang <806628409@qq.com>
2026-07-01 13:14:03 +0800

    Make template_features optional in updateSpikeTimes
    Check for the presence of template_features.npy before reading or writing it in PhyManipulations/updateSpikeTimes.m. Update the header comment to note that template_features is optional and conditionally read/written to avoid errors when the file is missing. Also add final newline.

--- PhyManipulations/updateSpikeTimes.m
+++ PhyManipulations/updateSpikeTimes.m
@@ -12,7 +12,7 @@ function updateSpikeTimes(folder_data, spike_times_new)
 %       (3) amplitudes.npy
 %       (4) pc_features.npy
 %       (5) spike_templates.npy
-%       (6) template_features.npy
+%       (6) template_features.npy (if present)
 %
 
 % read all npy files
@@ -21,7 +21,11 @@ spike_times = readNPY(fullfile(folder_data, 'spike_times.npy'));
 spike_clusters = readNPY(fullfile(folder_data, 'spike_clusters.npy'));
 spike_templates = readNPY(fullfile(folder_data, 'spike_templates.npy'));
 pc_features = readNPY(fullfile(folder_data, 'pc_features.npy'));
-template_features = readNPY(fullfile(folder_data, 'template_features.npy'));
+path_template_features = fullfile(folder_data, 'template_features.npy');
+has_template_features = isfile(path_template_features);
+if has_template_features
+    template_features = readNPY(path_template_features);
+end
 
 % the spike times should be increasing values
 assert(length(spike_times) == length(spike_times_new));
@@ -32,5 +36,7 @@ writeNPY(amplitudes(idx_sort), fullfile(folder_data, 'amplitudes.npy'));
 writeNPY(spike_clusters(idx_sort), fullfile(folder_data, 'spike_clusters.npy'));
 writeNPY(spike_templates(idx_sort), fullfile(folder_data, 'spike_templates.npy'));
 writeNPY(pc_features(idx_sort, :, :), fullfile(folder_data, 'pc_features.npy'));
-writeNPY(template_features(idx_sort, :), fullfile(folder_data, 'template_features.npy'));
+if has_template_features
+    writeNPY(template_features(idx_sort, :), path_template_features);
+end
 end
